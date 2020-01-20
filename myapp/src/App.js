abea484f4ad33edbbdd891a8b21ac763e8be9e8e
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-01-20 10:55:10 -0500

    12components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <h1>React App</h1>
       <p>Welcome</p>
+      <Ninjas />
     </div>
   );
 }
-
+}
 export default App;
