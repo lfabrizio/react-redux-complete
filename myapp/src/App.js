60d66ab00eef28325306f9292f33b5ada3c42f90
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-01-25 18:31:12 -0500

    functionsasprops

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -9,13 +9,20 @@ class App extends Component {
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 },
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas:
+    })
+  }
   render() {
   return (
     <div className="App">
       <h1>React App</h1>
       <p>Welcome</p>
       <Ninjas ninjas={this.state.ninjas} />
-      <AddNinja />
+      <AddNinja addNinja={this.addNinja}/>
     </div>
   );
 }
