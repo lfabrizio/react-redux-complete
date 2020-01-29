c3ef44856d5e402dba8c99b375e758c91b344744
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-01-28 19:52:34 -0500

    27Programaticredirects

--- poketimes/src/components/Contact.js
+++ poketimes/src/components/Contact.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
-const Contact = () => {
+const Contact = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // }, 2000);
     return (
         <div clasName="container">
             <h4 clasName="center">Contact</h4>
