66fbfb7d55c07aef5fae2aca743e54fcd975e0a2
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-02-01 11:54:39 -0500

    29usingaxios

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Home = () => {
+class Home extends Component {
+    state = {
+        posts: [ ]
+    }
+    componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res => {
+        console.log(res)
+        this.ListeningStateChangedEvent({
+            posts: res.data.slice(0, 10)
+        })
+    })
+}
+
+    render () {
+        const { post } = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className="post card" key={posts.id}>
+                        <div className="card-content">
+                            <span className="card-title">{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center">No posts </div>
+        )
     return (
         <div clasName="container">
             <h4 clasName="center">Home</h4>
-            <p>Paragraph </p>
+            <p>{postList} </p>
         </div>
     )
 }
-
+}
 export default Home
