d1c97728409a69561f500834d788f58a077dce90
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-02-05 21:02:39 -0500

    41

--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -1,7 +1,7 @@
 const initState = {
     posts: [
-        {id: '1', title: 'Title1', body: 'some text here '}
-        {id: '2', title: 'Title2', body: 'some more text here '}
+        {id: '1', title: 'Title1', body: 'some text here '},
+        {id: '2', title: 'Title2', body: 'some more text here '},
         {id: '3', title: 'Title3', body: 'more and more text here '}
     ]
 }
